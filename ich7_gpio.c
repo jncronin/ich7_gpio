4b190fc07e9fb714cb5f731d50056439f4bbc7c8
John Cronin <johncronin@scifa.co.uk>
2011-01-03 16:34:56 +0000

    Added an include for spinlock.h which is required before leds.h on old kernels

--- ich7_gpio.c
+++ ich7_gpio.c
@@ -23,6 +23,7 @@
 #include <linux/init.h>
 #include <linux/ioport.h>
 #include <linux/kernel.h>
+#include <linux/spinlock.h>
 #include <linux/leds.h>
 #include <linux/module.h>
 #include <linux/pci.h>
